c49a02ef96efa5343090c1877370ce8c54d88cfc
Benoît Léo Maillard <benoitleo.maillard@gmail.com>
2015-04-24 22:41:52 +0000

    Fixed bug

--- remote/views.py
+++ remote/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect, get_object_or_404
 import os
-from remote.omx_api import *
+import remote.omx_api as api
 
 # Create your views here.
 def player(request):
@@ -9,7 +9,7 @@ def player(request):
     file_path = request.GET["file"]
     subtitles_path = request.GET.get("subtitles", "")
     
-    launch(file_path, subtitles_path)
+    api.launch(file_path, subtitles_path)
     
     return render(request, "remote/main.html", {"file" : file_path})
     
